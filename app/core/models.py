332b8eea4a8205f6feb2e5ac12b1fe36c025701b
Gilles <gilles.guglielmoni@gmail.com>
2021-10-31 08:57:01 +0100

    add Tags feature

--- app/core/models.py
+++ app/core/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
         PermissionsMixin
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-
         """ creates and saves a new user """
         if not email:
             raise ValueError('Users must have an email address')
@@ -16,7 +17,6 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-
         """ creates and saves a new superuser """
         user = self.create_user(email, password)
         user.is_staff = True
@@ -26,8 +26,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-
     """ custom user model that supports using email instead of username """
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -36,3 +36,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """ Tag to be used for a recipe """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
